a2b5d5e1c57fe2fa54521538b3de59b1d096eb88
ubuntu <hanna.tseran@gmail.com>
2016-01-04 00:32:54 +0300

    paths + mongodb init

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 NGXCFGFOLDER=$(pwd)
 sudo nginx -c $(echo $NGXCFGFOLDER"/nginx.cfg")
 sudo nginx -s reopen
-g++ hello.cpp -lfcgi++ -lfcgi -o videohandler
+g++ main.cpp tva.cpp -lfcgi++ -lfcgi -I$HOME/mongo-client-install/include -L$HOME/mongo-client-install/lib -lmongoclient -lboost_thread -lboost_system -lboost_regex -o videohandler
 spawn-fcgi -p 8000 -n videohandler
